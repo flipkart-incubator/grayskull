7d6c46c752109f6d804510f9a92644ce3e09be47
Sai Sandeep Rangisetti <sandeep.sai@flipkart.com>
2025-10-08 21:29:20 +0530

    Added unit tests

--- derby-async-audit/src/main/java/com/flipkart/grayskull/app/audit/AuditProperties.java
+++ derby-async-audit/src/main/java/com/flipkart/grayskull/app/audit/AuditProperties.java
@@ -2,13 +2,19 @@ package com.flipkart.grayskull.app.audit;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "audit")
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
 public class AuditProperties {
     /**
      * Folder for Apache Derby where audit events are stored
