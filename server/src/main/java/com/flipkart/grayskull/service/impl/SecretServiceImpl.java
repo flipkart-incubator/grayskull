de661d297e6cb9dc993dafd4e82a7ac003230624
imanadi <anadivyas2100@gmail.com>
2025-10-17 16:03:47 +0530

    Update SecretServiceImpl.java
    Remove @Transactional(readOnly = true)

--- server/src/main/java/com/flipkart/grayskull/service/impl/SecretServiceImpl.java
+++ server/src/main/java/com/flipkart/grayskull/service/impl/SecretServiceImpl.java
@@ -37,7 +37,6 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@Transactional(readOnly = true)
 public class SecretServiceImpl implements SecretService {
 
     private final SecretRepository secretRepository;
